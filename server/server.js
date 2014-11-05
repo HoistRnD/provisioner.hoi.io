2c85975e03b6d9ab70266b68c359ea4b07b5880f
Amelia Laundy <amelialaundy@gmail.com>
2014-11-05 17:00:05 +1300

    correct view engine set to hbs

--- server/server.js
+++ server/server.js
@@ -1,13 +1,22 @@
 var Hapi = require('hapi');
 var Good = require('good');
+var Path = require('path');
 
 var server = new Hapi.Server(3000);
+
+server.views({
+    engines: {
+        hbs: require('handlebars')
+    },
+    path: Path.join(__dirname, '/templates'),
+});
 
 server.route({
   method: 'GET',
   path: '/',
   handler: function (request, reply) {
-    reply('Hello, world!');
+    
+    reply.view('welcome.hbs', {});
   }
 });
 
