f1966ab8e5aab5bd2eb9a600d63608b5aa0a8763
Amelia Laundy <amelialaundy@gmail.com>
2014-11-13 09:21:28 +1300

    make db connection tied to server start/stop (fixes#4)

--- server/stop_server.js
+++
@@ -1,8 +0,0 @@
-var server = require('./server.js')
-var mongoose = require('mongoose');
-var config = require('config')
-
-server.start(function () {
-  mongoose.connect(config.get('Hoist.mongo.db'));
-  console.log('info', 'Server running at: ' + server.info.uri);
-});
